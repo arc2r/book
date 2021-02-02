28344c5fa71fb992f7ed14cd18f8f39711ebdc36
Nils Ratnaweera <rata@zhaw.ch>
2021-02-02 12:12:32 +0100

    see https://github.com/rstudio/bookdown/issues/418

---
+++ R/clear_vars_and_pkgs.R
@@ -0,0 +1,9 @@
+# Source: https://github.com/rstudio/bookdown/issues/418#issuecomment-314265432
+
+# detach packages
+packages = sapply(sessionInfo()$otherPkgs, function(x) x$Package)
+packages = packages[packages != "bookdown"]
+packages = sapply(packages, function(p) paste0("package:", p))
+lapply(packages, detach, character.only = TRUE, unload = TRUE)
+# clear environment
+rm(list = ls())
