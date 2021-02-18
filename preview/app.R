6353960ff2f10c61f328f138720c158452d9e79d
Nils Ratnaweera <rata@zhaw.ch>
2021-02-18 12:33:21 +0100

    started on a preview app, than should be outsourced as an addin

---
+++ preview/app.R
@@ -0,0 +1,67 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(yaml)
+
+
+lis <- read_yaml("_rmd_files.yaml")
+
+part <- "Geoprocessing Tools"
+chapter <- "Analysis"
+
+
+
+df <- lis[[part]]$chapters[[chapter]]
+
+rmds <- c("index.Rmd",lis[[part]]$index, file.path(df$folder,df$subchapters))
+
+
+bookdown::render_book(rmds, preview = TRUE)
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
